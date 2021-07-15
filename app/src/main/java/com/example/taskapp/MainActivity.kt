8c2d767f1c162b3e72bfa00085e17d7eed93f603
Velka-code <nadaya081@gmail.com>
2021-07-15 23:36:05 +0900

    検索機能追加

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.taskapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 import io.realm.RealmChangeListener
 import io.realm.Sort
-import java.util.*
+import android.content.Intent
+import androidx.appcompat.app.AlertDialog
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.util.Log
+import kotlinx.android.synthetic.main.content_input.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCategory =""//追加
+
     private lateinit var mRealm: Realm
     private val mRealmListener = object : RealmChangeListener<Realm> {
         override fun onChange(element: Realm) {
@@ -19,13 +26,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mTaskAdapter: TaskAdapter
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val intent = Intent(this, InputActivity::class.java)
+            startActivity(intent)
         }
 
         // Realmの設定
@@ -36,28 +44,84 @@ class MainActivity : AppCompatActivity() {
         mTaskAdapter = TaskAdapter(this)
 
         // ListViewをタップしたときの処理
-        listView1.setOnItemClickListener { parent, view, position, id ->
+        listView1.setOnItemClickListener { parent, _, position, _ ->
             // 入力・編集する画面に遷移させる
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
         }
 
         // ListViewを長押ししたときの処理
-        listView1.setOnItemLongClickListener { parent, view, position, id ->
+        listView1.setOnItemLongClickListener { parent, _, position, _ ->
             // タスクを削除する
+            val task = parent.adapter.getItem(position) as Task
+
+            // ダイアログを表示する
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "を削除しますか")
+
+            builder.setPositiveButton("OK"){_, _ ->
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+
+                val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+                val resultPendingIntent = PendingIntent.getBroadcast(
+                    this,
+                    task.id,
+                    resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+                alarmManager.cancel(resultPendingIntent)
+
+                reloadListView()
+            }
+
+            builder.setNegativeButton("CANCEL", null)
+
+            val dialog = builder.create()
+            dialog.show()
             true
         }
-
-        // アプリ起動時に表示テスト用のタスクを作成する
-        addTaskForTest()
+        //検索ボタンを押した時の処理　追加
+        explorer_button.setOnClickListener {
+            setCategory()
+            //Log.d("test","カテゴリ：" + getCategory())
+            reloadListView()
+        }
 
         reloadListView()
     }
 
-    private fun reloadListView() {
-        // Realmデータベースから、「すべてのデータを取得して新しい日時順に並べた結果」を取得
-        val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
+    public fun setCategory(){
+        this.mCategory = category_input_text.text.toString()
+    }
 
-        // 上記の結果を、TaskListとしてセットする
-        mTaskAdapter.mTaskList = mRealm.copyFromRealm(taskRealmResults)
+    public fun getCategory(): String {
+        return this.mCategory
+    }
+
+
+    //ここが一覧画面に表示する箇所
+    private fun reloadListView() {
+
+        if(getCategory() !=""){
+            val taskRealmResults = mRealm.where(Task::class.java).equalTo("category",getCategory()).findAll()
+            // 上記の結果を、TaskListとしてセットする
+            mTaskAdapter.mTaskList = mRealm.copyFromRealm(taskRealmResults)
+        }else{
+            // Realmデータベースから、「すべてのデータを取得して新しい日時順に並べた結果」を取得
+            val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
+            // 上記の結果を、TaskListとしてセットする
+            mTaskAdapter.mTaskList = mRealm.copyFromRealm(taskRealmResults)
+        }
 
         // TaskのListView用のアダプタに渡す
         listView1.adapter = mTaskAdapter
@@ -72,14 +136,7 @@ class MainActivity : AppCompatActivity() {
         mRealm.close()
     }
 
-    private fun addTaskForTest() {
-        val task = Task()
-        task.title = "作業"
-        task.contents = "プログラムを書いてPUSHする"
-        task.date = Date()
-        task.id = 0
-        mRealm.beginTransaction()
-        mRealm.copyToRealmOrUpdate(task)
-        mRealm.commitTransaction()
+    companion object {
+        const val EXTRA_TASK = "com.example.taskapp.TASK"
     }
 }
--- app/src/main/java/com/example/taskapp/Task.kt
+++ app/src/main/java/com/example/taskapp/Task.kt
@@ -4,13 +4,16 @@ import java.io.Serializable
 import java.util.Date
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
+import kotlinx.android.synthetic.main.content_input.*
 
 open class Task : RealmObject(), Serializable {
     var title: String = ""      // タイトル
     var contents: String = ""   // 内容
     var date: Date = Date()     // 日時
+    var category :String = "" //カテゴリ　追加
 
     // idをプライマリーキーとして設定
     @PrimaryKey
     var id: Int = 0
+
 }
